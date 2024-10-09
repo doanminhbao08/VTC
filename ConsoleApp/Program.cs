4d89409421410b76c8e691fffddd5758fdd56655
unknown <you@example.com>
2024-10-09 15:26:50 +0700

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, VTC");
+
+            Console.WriteLine("Your name:");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + ", welcome to VTC");
+
+            GreetUser(name);
+        }
+
+        static void GreetUser(string? name)
+        {
+            Console.WriteLine($"Hi, {name}!");
+        }
+    }
+}
+
